b7e8ac3b61c542811bcf6fbdb613869460dcc512
Emerson Costa <costtasemerson@gmail.com>
2018-05-26 09:27:12 -0300

    concurrently catching info

--- ch1/fetch.go
+++ ch1/fetch.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"io"
 	"net/http"
 	"os"
 	"strings"
 )
 
-func main() {
+func fetch1() {
 	for _, url := range os.Args[1:] {
 		if !strings.HasPrefix(url, "http://") {
 			url = "http://" + url
@@ -18,7 +17,7 @@ func main() {
 		if err != nil {
 			os.Exit(1)
 		}
-		fmt.Println(resp.StatusCode)
+
 		io.Copy(os.Stdout, resp.Body)
 		resp.Body.Close()
 	}
